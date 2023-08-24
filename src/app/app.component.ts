0a60bde317b6c8fc4de27d91e0dad4d201cfd157
Shivam-Singh-12 <shivamcs084@gmail.com>
2023-08-24 14:39:21 +0530

    @Input() & @Output() Transfer Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TransferData';
+
+  messageTochild = 'Hello Child!';
+  messageFromParent = 'Hello from Parent';
+  // P to C @Output()
+  messageFromChild1 = '';
+  messageFromChild2 = '';
+
+  handleMessageFromChild(messages: { message1: string; message2: string }) {
+    this.messageFromChild1 = messages.message1;
+    this.messageFromChild2 = messages.message2;
+  }
+
+  //
+  inputText = '';
+  submittedTexts: string[] = [];
+
+  onKeyUp(event: KeyboardEvent) {
+    this.inputText = (event.target as HTMLInputElement).value;
+  }
+
+  submitInput() {
+    if (this.inputText) {
+      this.submittedTexts.push(this.inputText);
+      this.inputText = ''; // Clear the input field after submission
+    }
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+
+  @Input() messageFromParent: string = "";
+  @Output() messageToParent = new EventEmitter<{ message1: string, message2: string }>();
+
+  sendMessageToParent() {
+    const message1 = "Hello Parent!";
+    const message2 = "Hello Parent2!";
+    this.messageToParent.emit({ message1, message2 });
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
